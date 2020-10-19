7de47619c1896f7e7d14d8df32b5bbe94a7d67e8
jasmyne-52 <usnunaf@gmail.com>
2020-10-19 18:48:34 -0400

    fixed the main function and author string

--- xword.py
+++ xword.py
@@ -2,21 +2,46 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Jasmyne Ford with help from Bethsheba"
 
-# YOUR HELPER FUNCTION GOES HERE
+
+def getDictionary():
+
+    dictionaryOpen = open('dictionary.txt', 'r')
+    dictionary = dictionaryOpen.read().split()
+    dictionaryOpen.close()
+    return dictionary
+
+
+dictionary = getDictionary()
+
+
+def checkWord(testword, dictionary):
+    nonBlanks = len(testword)-testword.count(' ')
+    for word in dictionary:
+        incLetter = 0
+        incMatch = 0
+        if len(word) == len(testword):
+            for letter in testword:
+                if letter == " ":
+                    incLetter += 1
+                elif letter == word[incLetter]:
+                    incLetter += 1
+                    incMatch += 1
+                else:
+                    incLetter += 1
+        if incMatch == nonBlanks:
+            print(word)
+    return
 
 
 def main():
-    with open('dictionary.txt') as f:
-        words = f.read().split()
-
-    test_word = raw_input(
+    testword = input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
-
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    getDictionary()
+    checkWord(testword, dictionary)
 
 
 if __name__ == '__main__':
     main()
+
